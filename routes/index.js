8b73266eab3c0d8b0757ab27625f91fdc1be9c6b
dylan horyza <horyza.dylan@gmail.com>
2023-11-08 11:29:46 -0700

    created delete route and comments

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-//index.js file
+// Require Express and the notes.js file
 const express = require('express');
 const notesRouter = require('./notes');
 const app = express();
 
+// Middleware to be aple to use the Notes Route
 app.use('/api/notes', notesRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-//server.js file
-
+// Require Express, path and the routes folder
 const express = require('express');
 const path = require('path');
 const notes = require('./routes');
@@ -7,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//Middleware
+//Middleware to use the Notes Route and the public folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', notes);
@@ -24,7 +23,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+// Listen to the designated PORT
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
 });
